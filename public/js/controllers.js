a40126e2be7c8e6b2d221198908082ff8862b3c0
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-07-02 10:58:29 -0700

    Starting game logic front-end angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,110 +1,71 @@
 'use strict';
 
 angular.module('myApp')
-  .controller('mainCtrl', function() {
-    console.log('mainCtrl!');
-  })
-  .controller('gameCtrl', function($scope) {
-    console.log('gameCtrl!');
-    $scope.sprites = [{
-      id: 1,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 2,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 3,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 4,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 5,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 6,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 7,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 8,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 9,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 10,
-      name: "bulbasaur",
-      imgUrl: "https://pokeapi.co/media/img/9.png "
-    },
-    {
-      id: 11,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 12,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 13,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 14,
-      name: "bulbasaur",
-      imgUrl: "https://pokeapi.co/media/img/9.png "
-    },
-    {
-      id: 15,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    },
-    {
-      id: 16,
-      name: "bulbasaur",
-      imgUrl: "http://pokeapi.co/media/img/1383571573.78.png"
-    }];
+.controller('mainCtrl', function() {
+  console.log('mainCtrl!');
+})
+.controller('gameCtrl', function($scope, $timeout) {
+  console.log('gameCtrl!');
+  $scope.sprites = [{
+    id: 1,
+    name: "bulbasaur",
+    imgUrl: "http://pokeapi.co/media/img/1383571573.78.png",
+    active: false
+  },
+  {
+    id: 2,
+    name: "bulbasaur",
+    imgUrl: "http://pokeapi.co/media/img/1383571573.78.png",
+    active: false
+  },
+  {
+    id: 3,
+    name: "bulbasaur",
+    imgUrl: "http://pokeapi.co/media/img/1383571573.78.png",
+    active: false
+  },
+  {
+    id: 4,
+    name: "bulbasaur",
+    imgUrl: "http://pokeapi.co/media/img/1383571573.78.png",
+    active: false
+  }]
 
 
-    $scope.guess = (index) => {
-      console.log("index: ",index);
-      
-    }
+  $scope.guess = (index) => {
+    console.log("index: ",index);
+  }
 
-    function shuffle(array) {
-      var i = 0, j = 0, temp = null;
-
-      for (i = array.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1))
-        temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-      };
-      return array;
+  $scope.start = () => {
+    $scope.playerArr = [];
+    function random() {
+      return Math.floor(Math.random() * 4);
     };
+    for (let iteration = 1; iteration < 5; iteration++) {
+      $scope.playerArr.push(random());
+    }
+    $scope.activate = (element) => {
+      element.active = true;
+      $timeout(function() {element.active = false}, 600);
+    }
+    console.log('$scope.playerArr:', $scope.playerArr);
 
-    $scope.shuffledSprites = shuffle($scope.sprites);
+    let count = 0;
+    $scope.sequence = (count) => {
+      $scope.disabled = true;
+      $scope.activate($scope.sprites[$scope.playerArr[count]]);
+      if (count < $scope.playerArr.length - 1) {
+        $timeout(function() {
+          $scope.sequence(count);
+        }, 600);
+      } else {
+        $scope.disabled = false;
+      }
+      count++;
+    }
+    $scope.sequence(count);
 
-  })
+  }
+
+
+})
